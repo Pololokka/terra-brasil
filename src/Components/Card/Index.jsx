d500ed1092e26def87373b703cf7e662ca8f929d
Pololokka <vitorbulbovas1@hotmail.com>
2023-03-26 13:18:29 -0300

    :sparkles: feat: feitos cards da página principal

--- src/Components/Card/Index.jsx
+++ src/Components/Card/Index.jsx
@@ -1,27 +1,17 @@
 import "./Styles.css";
 import { useState } from "react";
 
-const Card = () => {
+const Card = ({ title, text, className }) => {
   const [hover, setHover] = useState(false);
 
   return (
     <div
-      className="card__container"
+      className={`card__container ${className}`}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
-      <h2 className="subtitulo subtitulo-hover">Placeholder Card Individual</h2>
-      {hover && (
-        <p className="texto">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id semper
-          massa, molestie hendrerit quam. Nam vel pretium augue. Cras quis
-          varius odio. Pellentesque fermentum ligula tortor. Pellentesque
-          habitant morbi tristique senectus et netus et malesuada fames ac
-          turpis egestas. Donec at venenatis ligula. Sed ut accumsan ipsum, eu
-          tincidunt neque. Nam consequat placerat sapien scelerisque vestibulum.
-          Donec molestie augue sed mauris vehicula sagittis.
-        </p>
-      )}
+      <h3 className="subtitulo subtitulo-hover">{title}</h3>
+      {hover && <p className="texto">{text}</p>}
     </div>
   );
 };
